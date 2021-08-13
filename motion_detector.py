32799bb5b1f04aaee25ee609bb3ec57af8f3a934
MirkoMilanovic <milanovic_mirko@ymail.com>
2021-08-13 11:14:42 +0200

    Second commit

--- motion_detector.py
+++ motion_detector.py
@@ -12,18 +12,29 @@ MOTION DETECTION:
             cv2.GaussianBlur(gray, (21, 21), 0) - img, gaussian kernel 21x21, standard deviation-commonly used 0
         - we make a delta_frame as a absolute difference of two frames:
             cv2.absdiff(first_frame, gray)
-4. make a treshold to make a only black and white inside the loop
-5. find the contours of the white parts of the img
-6. make a for loop for the contours, if the area is too small, not to consider it
-7. draw the rectangles over the big white areas, that are shown over the current video (frame)
-8. the time when the object enters/exits the screen
+4. make a treshold to make a only black and white inside the loop:
+            cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+        - of the delta frame, with a threshold=30, maxvalue=255, binary threshold method
+        (you get a tuple of 2 values, the first one is used for other methods, and the second is frame)
+5. to kick out small black areas inside the white areas, to smooth out the thresh_frame
+            thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
+        - thrash frame we smooth with no kernel, 2 iteratations used (more->smoother)
+6. find the contours of the white parts of the img
+            (_,cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            (cnts,_) - it depends on the cv2/cv3, p2/p3
+        - to find a contours of the image we use copy of the frame, not to modify it, retrieve ext. is
+        the method to get external lines, and a method for  approximation
+7. make a for loop for the contours, if the area is too small, not to consider it
+        - we need to iterate through the extracted contours, to exclude the small ones (small areas):
+        "if cv2.contourArea(contour) < 1000: .... continue"; continue-break the iteration and continue the loop,
+8. draw the rectangles over the big white areas, that are shown over the current video (frame)
+        "(x, y, w, h) = cv2.boundingRect(contour)" - we extract the rectangle values of the bounding rectangle
+9. the time when the object enters/exits the screen
 
 """
 import cv2, time
 
 first_frame = None
-video2 = cv2.VideoCapture(0)
-time.sleep(3)
 video = cv2.VideoCapture(0)
 
 
@@ -36,10 +47,22 @@ while True:
         cv2.imshow("First frame", gray)
         continue
 
-    cv2.imshow("Video", gray)
-
     delta_frame = cv2.absdiff(first_frame, gray)
+    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+    (cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:      
+        if cv2.contourArea(contour) < 1000:
+            continue      
+        (x, y, w, h) = cv2.boundingRect(contour)  
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+    cv2.imshow("Video", gray)
+    cv2.imshow("Threshold frame", thresh_frame)
     cv2.imshow("Difference", delta_frame)
+    cv2.imshow("Color frame", frame)
 
     key = cv2.waitKey(20)
     if key == ord('q'):
